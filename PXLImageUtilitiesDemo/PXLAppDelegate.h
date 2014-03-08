efda6fab1460b1668732199bca88b8cf9985d9d1
Jason Silberman <jasonjt99@gmail.com>
2014-03-08 12:24:15 -0800

    Initial Commit

---
+++ PXLImageUtilitiesDemo/PXLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PXLAppDelegate.h
+//  PXLImageUtilitiesDemo
+//
+//  Created by Jason Silberman on 3/8/14.
+//  Copyright (c) 2014 Jason Silberman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PXLImageUtilitiesDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PXLImageUtilitiesDemo
+//
+//  Created by Jason Silberman on 3/8/14.
+//  Copyright (c) 2014 Jason Silberman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXLAppDelegate class]));
+    }
+}
